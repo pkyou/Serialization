e4aa4b4652bc18aa37e2dfc7ecacb089760996ba
庞抗 <471162774@qq.com>
2016-12-13 09:58:36 +0800

    add class

---
+++ Serialise/SerializerTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serializer;
+
+namespace SerializerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestClass testClass = new TestClass()
+            {
+                TestNumber = 123,
+                TestString = "test"
+            };
+            Serialization serialization=new Serialization();
+            serialization.SerializeToFile(testClass, Environment.CurrentDirectory,"test.txt");
+
+            TestClass deserialiedClass =
+                serialization.Deserialize<TestClass>(Environment.CurrentDirectory + "\\test.txt");
+            Assert.AreEqual(123,deserialiedClass.TestNumber);
+            Assert.AreEqual("test",deserialiedClass.TestString);
+        }
+    }
+
+    [Serializable]
+    class TestClass
+    {
+        public string TestString { get; set; }
+        public int TestNumber { get; set; }
+
+    }
+}
---
+++ Serializer/Serialization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer
+{
+    public class Serialization
+    {
+        public void SerializeToFile<TSource>(TSource source, string path, string name)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Stream stream = new FileStream(path + "\\" + name, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, source);
+            stream.Close();
+        }
+
+        public TResult Deserialize<TResult>(string path)
+        {
+            Stream stream = new FileStream(path,
+                FileMode.Open, FileAccess.Read, FileShare.None);
+            IFormatter formatter = new BinaryFormatter();
+            TResult result = (TResult)formatter.Deserialize(stream);
+            stream.Close();
+            return result;
+        }
+    }
+}
